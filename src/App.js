9aca1f3e3b9efa1150b9671e51c0f0c76e7cf981
Garrett Yaworski <garrett@Garretts-MacBook-Pro.local>
2018-12-14 14:27:26 -0600

    redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Provider } from 'react-redux'
 import GuessedWords from './GuessedWords'
 import Congrats from "./Congrats"
+import store from './store'
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Jotto</h1>
-        <Congrats success={true}/>
-        <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]}/>
-      </div>
+      <Provider store={store}>
+        <div className="container">
+          <h1>Jotto</h1>
+          <Congrats success={true}/>
+          <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]}/>
+        </div>
+      </Provider>
     );
   }
 }
